511a28a97d95967eb234bc30a167470553613c57
i528667 <gagan.prajapati@sap.com>
2023-12-09 00:06:19 +0530

    working fine in backend

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+require('dotenv').config()
+
+
+const db = mysql.createConnection({
+  host: 'database-2.crus4vmhm48u.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'database_1',
+  port:'3306'
+});
+// Establish the connection
+db.connect((err) => {
+  if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(req.body, "in");
+    cb(null, `${req.body.productId}${path.extname(file.originalname)}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/uploads', express.static(path.join(__dirname ,'uploads')));
+
+
+app.post("/thumbnailUpload", upload.single("productThumbnail"), (req, res) => {
+  try {
+    console.log(req.file) ;
+    return res.json({ data: req.file.filename });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+app.get("/products", (req, res) => {
+    const q = "select * from product";
+    db.query(q, (err, data) => {
+      console.log(err, data);
+      if (err) return res.json({ error: err.sqlMessage });
+      else return res.json({ data });
+    });
+  });
+  app.post("/products", (req, res) => {
+    const q = `insert into product(productId, productTitle, productDescription, productPrice, availableQuantity, productThumbnail)
+      values(?)`;
+    const values = Object.values(req.body);
+    console.log("insert", values);
+    db.query(q, [values], (err, data) => {
+      console.log(err, data);
+      if (err) return res.json({ error: err.sqlMessage });
+      else return res.json({ data });
+    });
+  });
+  
+  app.get("/products/:productId", (req, res) => {
+    const id = req.params.productId;
+    const q = "SELECT * FROM product where productId=?";
+    db.query(q, [id], (err, data) => {
+      console.log(err, data);
+      if (err) return res.json({ error: err.sqlMessage });
+      else return res.json({ data });
+    });
+  });
+  
+  app.put("/products/:productId", (req, res) => {
+    const id = req.params.productId;
+    console.log("updated " + req.body);
+    const data = req.body;
+    if (data.productPrice) data.productPrice = Number.parseInt(data.productPrice);
+    if (data.availableQuantity)
+      data.availableQuantity = Number.parseInt(data.availableQuantity);
+    const q =
+      "update product set " +
+      Object.keys(data)
+        .map((k) => `${k} = ?`)
+        .join(",") +
+      " where productId='" +
+      id +
+      "'";
+    console.log(q);
+    db.query(q, [...Object.values(data)], (err, out) => {
+      console.log(err, out);
+      if (err) return res.json({ error: err.message });
+      else {
+        return res.json({ data: out });
+      }
+    });
+  });
+  
+  app.delete("/products/:productId", (req, res) => {
+    const id = req.params.productId;
+    console.log("deleting " + id, req.body);
+    const { productThumbnail } = req.body;
+    console.log(req.body);
+    const q = `DELETE FROM product WHERE productId= ?`;
+    db.query(q, [id], (err, data) => {
+      console.log(err, data);
+      if (err) return res.json({ error: err.sqlMessage });
+      else res.json({data})
+    })
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is listening" , 3000);
+});
